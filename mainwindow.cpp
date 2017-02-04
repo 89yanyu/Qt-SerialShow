654edf511648fb598c6e00b3741d230d47b7216a
89yanyu <995239101@qq.com>
2017-02-04 17:19:28 +0800

    获取SerialPortList

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,41 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSerialPort>
+#include <QSerialPortInfo>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    m_TimerId = startTimer(1000);
+}
+
+void MainWindow::UpdateComboList()
+{
+    QList<QSerialPortInfo> PortList = QSerialPortInfo::availablePorts();
+    ui->comboBox->clear();
+    foreach (auto i, PortList)
+    {
+        ui->comboBox->addItem(i.portName());
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    killTimer(m_TimerId);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+//    ui->textBrowser->setLineWrapMode(QTextBrowser::NoWrap);
+//    ui->textBrowser->insertPlainText(ui->textEdit->toPlainText());
+//    ui->textEdit->setPlainText("");
+}
+
+void MainWindow::timerEvent(QTimerEvent *event)
+{
+    if (ui->comboBox->isEnabled()) return;
+    UpdateComboList();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,6 +14,13 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private:
+    void UpdateComboList();
+    int m_TimerId;
+    void timerEvent(QTimerEvent *event);
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
